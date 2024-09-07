5d10f07e2d7e48f5834a0e8a6be9ba9078f44cc4
axel ludwig <axel.ludwig@outlook.com>
2024-09-07 22:48:52 +0200

    add start, stop buttons and status

---
+++ main.js
@@ -0,0 +1,95 @@
+const { error } = require('console');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5000; // Ton port
+
+
+var exec = require('child_process').exec;
+// Configurer express pour servir les fichiers statiques depuis le dossier public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pour servir index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+let getStatusCommand = "pm2 pid bot-soundboard-back";
+let startCommand = "pm2 start bot-soundboard-back";
+let stopCommand = "pm2 stop bot-soundboard-back";
+
+// Configurer express pour servir les fichiers statiques depuis le dossier public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pour servir index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/status', (req, res) => {
+    let response;
+    executeCommand(getStatusCommand).then(output => {
+        console.log("error");
+
+        if (output.includes('Error') || output.includes('Erreur')) {
+            throw new Error(output);
+        }
+
+        response = { status: 'error', message: output };
+        res.send(response);
+
+    }).catch(err => {
+        response = { status: 'error', message: err };
+        res.send(response);
+    });
+
+
+    // execute(getStatusCommand, function (output) { res.send(output); });
+})
+
+app.get('/start', (req, res) => {
+    executeCommand(startCommand).then(output => {
+        res.send(output);
+    }).catch(err => {
+        res.send(err);
+    });
+
+    // console.log('Starting server');
+    // execute(startCommand, function (output) { console.log(output); });
+})
+
+app.get('/stop', (req, res) => {
+    executeCommand(stopCommand).then(output => {
+        res.send(output);
+    }).catch(err => {
+        res.send(err);
+    });
+
+    // execute(stopCommand, function (output) { console.log(output); });
+})
+
+
+// function execute(command, callback) {
+//     exec(command, function (error, stdout, stderr) { callback(stdout); });
+// };
+
+function executeCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(`Erreur lors de l'exécution de la commande : ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                reject(`Erreur dans la commande : ${stderr}`);
+                return;
+            }
+            resolve(stdout);
+        });
+    });
+}
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur http://localhost:${PORT}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,60 @@
+getStatus();
+setInterval(getStatus, 1000);
+
+let isRunning = false;
+
+function getStatus() {
+    fetch('http://localhost:5000/status').then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+
+        if (data.status === 'error') {
+            isRunning = false;
+        } else {
+            isRunning = true;
+        } return;
+
+        // if (data === '') console.log('Server is down');
+        // else console.log('Server is up');
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function start() {
+    alert('Starting server');
+    fetch('http://localhost:5000/start').then(response => {
+        return response.text();
+    }).then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function stop() {
+    alert('Stopping server');
+    fetch('http://localhost:5000/stop').then(response => {
+        return response.text();
+    }).then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    })
+};
+
+// Fonction pour afficher le texte en fonction de l'état du serveur
+function afficherEtatServeur() {
+    const etatServeur = document.getElementById("etat-serveur");
+    if (isRunning) {
+        etatServeur.innerHTML = "Le serveur Fossason est actuellement <strong>EN MARCHE</strong>.";
+        etatServeur.style.color = "green";
+    } else {
+        etatServeur.innerHTML = "Le serveur Fossason est actuellement <strong>HORS SERVICE</strong>.";
+        etatServeur.style.color = "red";
+    }
+}
+
+// Exécuter la fonction une fois la page chargée
+window.onload = afficherEtatServeur;
